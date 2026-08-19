59c3651b671764f9beed85672a6533042457aa71
adel31moum <adelmoum3@gmail.com>
2026-08-20 00:10:19 +0100

    add src/pages/Join.jsx

---
+++ src/pages/Join.jsx
@@ -0,0 +1,9 @@
+import OnboardingWizard from '../components/OnboardingWizard';
+
+export default function Join() {
+  return (
+    <div className="bg-parchment min-h-[80vh]">
+      <OnboardingWizard />
+    </div>
+  );
+}
