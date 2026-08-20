734e613bc98155c5d71d96eeb699d34431314888
adel31moum <adelmoum3@gmail.com>
2026-08-20 06:36:39 +0100

    update src/pages/Join.jsx (adaptive path + local backend + marriage identity)

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -2,7 +2,7 @@ import OnboardingWizard from '../components/OnboardingWizard';
 
 export default function Join() {
   return (
-    <div className="bg-parchment min-h-[80vh]">
+    <div className="bg-parchment min-h-[80vh] oil-bg oil-join">
       <OnboardingWizard />
     </div>
   );
